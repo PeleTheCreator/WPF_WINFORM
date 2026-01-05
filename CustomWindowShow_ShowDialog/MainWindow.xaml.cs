e69cf67f1938d56ed24fdc1493c95d9c43d56b12
Mukaila Oluwapelumi Salami <salamioluwapelumi99@gmail.com>
2026-01-05 06:06:25 +0000

    modal

---
+++ CustomWindowShow_ShowDialog/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomWindowShow_ShowDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void modalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ModalWindow modalWindow = new ModalWindow();
+            modalWindow.Owner = this;
+            Opacity = 0.4;
+            modalWindow.ShowDialog();
+            Opacity = 1;
+            inputTxt.Text = modalWindow.success ? modalWindow.inputTxt : string.Empty;      
+        }
+
+        private void normalBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NormalWindow normalWindow = new NormalWindow();
+            normalWindow.Show();
+        }
+    }
+}
---
+++ CustomWindowShow_ShowDialog/ModalWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomWindowShow_ShowDialog
+{
+    /// <summary>
+    /// Interaction logic for ModalWindow.xaml
+    /// </summary>
+    public partial class ModalWindow : Window
+    {
+        public bool success = false;
+        public string inputTxt = string.Empty;
+        public ModalWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            success = true;
+            inputTxt = modalInputTxt.Text;
+            Close();
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            success = false;
+            Close();
+        }
+
+        private void modalInputTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (modalInputTxt.Text.Length > 0)
+            {
+                okBtn.IsEnabled = true;
+            }
+            else
+            {
+                okBtn.IsEnabled = false;
+            }
+        }
+    }
+}
